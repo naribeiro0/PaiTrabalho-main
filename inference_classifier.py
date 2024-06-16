ecdb99f626ea4115646e47059a9daa2f2506a665
naribeiro0 <anarafael2@icloud.com>
2024-06-16 20:39:21 -0300

    Terceiro commit

--- inference_classifier.py
+++ inference_classifier.py
@@ -30,12 +30,13 @@ hands = mp_hands.Hands(static_image_mode=False, min_detection_confidence=0.5, mi
 labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'}
 
 recognized_letters = []  # Lista para armazenar as letras reconhecidas
+previous_letter = ''  # Variável para armazenar a letra anterior
 
 while True:
     try:
         ret, frame = cap.read()
         if not ret:
-            print("Failed to grab frame xxx")
+            print("Failed to grab frame")
             continue
 
         H, W, _ = frame.shape
@@ -61,12 +62,19 @@ while True:
 
                 predicted_character = labels_dict[int(prediction[0])]
                 
-                if len(recognized_letters) == 0 or predicted_character != recognized_letters[-1]:
-                    recognized_letters.append(predicted_character)  # Adiciona a letra reconhecida à lista
+                # Adicionar a letra reconhecida à lista, evitando repetições consecutivas
+                if not recognized_letters or recognized_letters[-1] != predicted_character:
+                    recognized_letters.append(predicted_character)
+                    previous_letter = recognized_letters[-2] if len(recognized_letters) > 1 else ''  # Atualiza a letra anterior
 
                 cv2.putText(frame, predicted_character, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2, cv2.LINE_AA)
 
                 mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS, mp_drawing_styles.get_default_hand_landmarks_style(), mp_drawing_styles.get_default_hand_connections_style())
+
+        # Exibir a letra anterior reconhecida na janela do vídeo
+        if previous_letter:
+            cv2.putText(frame, "Letra anterior:", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(frame, previous_letter, (300, 100), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2, cv2.LINE_AA)
 
         cv2.imshow('frame', frame)
         key = cv2.waitKey(1)
@@ -82,7 +90,7 @@ cv2.destroyAllWindows()
 # Escrever as letras reconhecidas em um arquivo de texto
 output_file = "recognized_letters.txt"
 with open(output_file, 'w') as f:
-    f.write("Letras reconhecidas durante a execução (sem repetição em sequência):\n")
+    f.write("Letras reconhecidas durante a execução:\n")
     for letter in recognized_letters:
         f.write(f"{letter}")
 
